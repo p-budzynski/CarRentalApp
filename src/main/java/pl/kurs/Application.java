f412b46abbd4292aaf3f955b3cb5ee14d9133f56
p-budzynski <p.budzynski@op.pl>
2025-07-06 22:06:50 +0200

    fixed bugs

--- src/main/java/pl/kurs/Application.java
+++ src/main/java/pl/kurs/Application.java
@@ -3,10 +3,12 @@ package pl.kurs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class Application {
     public static void main(String[] args) {
